802f54cc5f1cad6a0a66c8c811d3af6286d9d424
LinaeSostra <rebeccadun@dun.org>
2018-06-14 13:47:49 -0700

    Add mockup test script

---
+++ test/testing/testing.ino
@@ -0,0 +1,13 @@
+
+//TODO(Rebecca): This will be a testing script of each sensor/actuator, 
+// but this is a sad file for the time being.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
