3e2c176f8b12cbbbf96c5911c2f256c372cea8c9
LinaeSostra <rebeccadun@dun.org>
2018-06-24 16:51:25 -0700

    Minor Refactoring

--- test/timeOfFlight.ino
+++ test/timeOfFlight.ino
@@ -1,13 +1,14 @@
 /*
  * Time of Flight
  */
-
 #include "Pins.h"
 
 ////////////////////////////////
 /* Functions */
 ////////////////////////////////
-uint16_t getDistance() {
+
+// Returns the distance of the time of flight in mm's
+uint16_t getTimeOfFlightReading() {
   VL53L0X_RangingMeasurementData_t measure;
 
   timeOfFlight.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
@@ -15,8 +16,9 @@ uint16_t getDistance() {
   return checkingIncorrectData ? measure.RangeMilliMeter : 0;
 }
 
+// Prints out the time of flight reading
 void testTimeOfFlight() {
-  uint16_t distance = getDistance();
+  uint16_t distance = getTimeOfFlightReading();
   if(distance == 0) {
     Serial.println("Out of Range.");
   } else { 
