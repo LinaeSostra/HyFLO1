a4d426b72fb8a4aedbdb6d2f3a06afce6ac8b0f6
LinaeSostra <rebeccadun@dun.org>
2018-06-24 16:21:26 -0700

    Added Time of Flight Testing

---
+++ test/Pins.h
@@ -0,0 +1,16 @@
+/* Pins */
+#ifndef PINS_H
+#define PINS_H
+
+// Time of Flight
+const int sdaPin = 2;
+const int sclPin = 3;
+
+// Ultrasonic Sensor
+const int triggerPin = 10;
+const int echoPin = 9;
+
+// Pump
+const int pumpPin = 11;
+
+#endif
--- test/test.ino
+++ test/test.ino
@@ -1,13 +1,55 @@
-
+/*
+ * The purpose of this script is to test each sensor/actuator 
+ * works as intended.
+ * 
+ * Written By: Rebecca Dun
+ */
 //TODO(Rebecca): This will be a testing script of each sensor/actuator, 
 // but this is a sad file for the time being.
 
+#include "libraries/VL53L0X/Adafruit_VL53L0X.h"
+#include "Pins.h"
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+#define DEBUG // comment this line to disable debug (Serial Prints)
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  while(!Serial) {
+    delay(1);
+  }
+
+#ifdef DEBUG
+  Serial.println("Adafruit VL53L0X test");
+  if(!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+
+  Serial.println("Adafruit VL53L0X Booted Up Successfully");
+#endif
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  testTimeOfFlight();
+
+  delay(100);
 
 }
+
+void testTimeOfFlight() {
+  VL53L0X_RangingMeasurementData_t measure;
+
+  lox.rangingTest(&measure, false);
+
+  if (measure.RangeStatus != 4) {
+    Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+  } else {
+    Serial.println("Out of Range.");
+  }
+}
+
