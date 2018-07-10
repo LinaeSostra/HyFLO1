8f4e11569767fe43c030e5818af06db7d83ae9f8
LinaeSostra <rebeccadun@dun.org>
2018-07-09 23:42:05 -0700

    Main Code Cleanup/Reordering

--- main/HyFLO1/HyFLO1.ino
+++ main/HyFLO1/HyFLO1.ino
@@ -4,51 +4,18 @@
  * The purpose of this script is to automate the HyFLO 1 to center over an arbitrary cup, and 
  * dispense the user's preference of liquid using Fuzzy Logic.
  */
-#include "libraries/SparkFun_VL6180X.h"
+#include "src/VL53L0X/Adafruit_VL53L0X.h"
+#include "src/NewPing/NewPing.h"
+#include "Pins.h"
 
 #define DEBUG // comment this line to disable debug (Serial Prints)
 
-// Time of Flight Sensor
-#define TIME_OF_FLIGHT_ADDRESS 0x29
-#define TIME_OF_FLIGHT_MAX_DISTANCE 210 // mm
-
-VL6180x sensor(TIME_OF_FLIGHT_ADDRESS);
-
-// Ultrasonic Sensor
-/*
- * The ultrasonic sensor requires 2 pins (http://wiki.jmoon.co/hcsr04/):
- * Trigger Pin is used to send out an ultrasonic high level pulse for at least 10 microseconds
- * Echo Pin automatically detects the returning pulse, measured in microseconds
- */
-#define triggerPin 10
-#define echoPin 9
-
-#define SPEED_OF_SOUND 0.343 // mm per microsecond
-#define DETECTION_THRESHOLD 100 // mm
-#define MAX_ERROR_PERCENTAGE 0.1 // % (unitless)
-
-#define TRIGGER_SWITCH_WAITTIME 2 // microseconds
-#define TRIGGER_PULSE_WAITTIME 10 // microseconds
-#define CONTAINER_DEBOUNCE_WAITTIME 1000 // milliseconds
-
-// Stepper Motor Easy Driver
-#define stepperPin 4 // Rising edge (LOW -> HIGH) triggers a step
-#define directionPin 5 // Set LOW to step 'forward', Set HIGH to step 'backwards'
-#define enablePin  6 // Controls whether GND is enabled
-
 #define STEPPER_SWITCH_WAITTIME 70 // microseconds
+#define MAX_ERROR_PERCENTAGE 0.1 // % (unitless)
 
 int stepCounter = 0; 
 
-// Tactile Position Switches
-// These tactile switches are HIGH when not pressed, and LOW when pressed
-#define homePin 7
-#define endPin 8
-
 bool hasReturnedHome = false;
-
-// Pump
-#define pumpPin 11
 
 // Other Global Variables
 
@@ -79,17 +46,13 @@ bool hasFinishedDispensing = false;
 void setup() {
   Serial.begin(BAUD_RATE);
 
-  // Initialize Time of Flight Sensor
-  if(sensor.VL6180xInit() != 0) {
-    Serial.println("FAILED TO INITIALIZE");
-  }
-  sensor.VL6180xDefautSettings();
+  while(!Serial) { delay(1); }
 
-  delay(1000);
+  // Initialize Time of Flight Sensor
+  timeOfFlightSetup();
 
   // Initialize Ultrasonic Sensor
-  pinMode(triggerPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  ultrasonicSetup();
 
   // Initialize Stepper Motor / Easy Driver
   pinMode(stepperPin, OUTPUT);
@@ -353,59 +316,4 @@ void resetSystem() {
   isNozzleCentered = false;
   hasReturnedHome = false;
   resetDriver();
-}
-
-// Returns the time of flight reading as a height
-int getTimeOfFlightReading() {
-  int height = TIME_OF_FLIGHT_MAX_DISTANCE - sensor.getDistance();
-  //assert(height >= 0);
-  return height;
-}
-
-// Returns the ultrasonic distance reading as a distance
-int getUltrasonicReading() {
-  digitalWrite(triggerPin, LOW); 
-  delayMicroseconds(TRIGGER_SWITCH_WAITTIME); // Waiting to update to LOW
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(TRIGGER_PULSE_WAITTIME); // Waiting for return signal
-  digitalWrite(triggerPin, LOW);
-
-  // Converting ultrasonic reading to distance
-  //
-  // Note: To convert the ultrasonic measurement from time (microseconds) to distance (mm),
-  // the time for the wave to return needs to be halved as it presents both the time to hit
-  // the object and return back to the sensor. Then this time needs to multipled by 
-  // the speed of sound. All of this is calculated as a float, and then casted into an int
-  // to prevent integer overflow/negative distance readings.
-  //
-  // distance = ((Time for wave to return / 2) * Speed of Sound)
-  int distance = (int) (pulseIn(echoPin, HIGH) / 2 * SPEED_OF_SOUND);
-  return distance;
-}
-
-// Checks whether an object has been placed in the vicinity or not
-bool checkForContainer() {
-  bool isContainerThere = false;
-  int ultrasonicDistance = getUltrasonicReading();
-  int temp = getUltrasonicReading();
-#ifdef DEBUG
-  Serial.print("Ultrasonic Distance: "); Serial.println(ultrasonicDistance);
-  Serial.print("Temp Distance: "); Serial.println(temp);
-#endif
-  
-  bool isObjectPresent = ultrasonicDistance < DETECTION_THRESHOLD;
-  if (isObjectPresent) {
-    // Debounce distance checking
-    delay(CONTAINER_DEBOUNCE_WAITTIME);
-    int ultrasonicDistance2 = getUltrasonicReading();
-    bool isDistancePositive = ultrasonicDistance > 2;
-    double errorPercentage = isDistancePositive ? (abs(ultrasonicDistance - ultrasonicDistance2) / double(ultrasonicDistance)) : 0; 
-    
-    // Checking if distance has stabilized
-    bool hasDistanceStabilized = errorPercentage < MAX_ERROR_PERCENTAGE;
-    if (hasDistanceStabilized) {
-      isContainerThere = true;
-    }
-  }
-  return isContainerThere;
 }
